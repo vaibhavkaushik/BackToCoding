b02e44632489f7d9523e3fcd2dec7cb655128559
VAIBHAV KAUSHIK <vaibhavkaushik23@gmail.com>
2021-08-29 22:21:22 +0530

    Create Tower_Of_Hanoi.java

---
+++ Tower_Of_Hanoi.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1 = scn.nextInt();
+        int t2 = scn.nextInt();
+        int t3 = scn.nextInt();
+        
+        toh(n, t1, t2, t3);
+    }
+
+    public static void toh(int n, int t1, int t2, int t3){
+        if(n == 0) return;
+        
+       
+        toh(n-1, t1, t3, t2);
+        
+        
+        System.out.println(n + "[" + t1 + " -> " + t2 + "]");
+        
+        
+        toh(n-1, t3, t2, t1);
+    }
+
+}

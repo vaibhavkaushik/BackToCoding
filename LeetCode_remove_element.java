822c90057b88bd15e784e5ccb1231bdfc55edf6f
VAIBHAV KAUSHIK <vaibhavkaushik23@gmail.com>
2021-09-01 00:28:49 +0530

    Create LeetCode_remove_element.java

---
+++ LeetCode_remove_element.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int counter = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val) nums[counter++] = nums[i];
+        }
+        return counter;
+    }
+}

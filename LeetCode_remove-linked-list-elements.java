f0d489586cfa746abd1c412619e92d1422e85280
VAIBHAV KAUSHIK <vaibhavkaushik23@gmail.com>
2021-10-10 01:30:17 +0530

    Create LeetCode_remove-linked-list-elements.java

---
+++ LeetCode_remove-linked-list-elements.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        if(head == null) return head;
+        
+        ListNode temp = head;
+        
+        while(temp!=null) {
+    
+            if(temp.next!=null && temp.next.val == val) {
+                temp.next = temp.next.next;
+            }
+            else {
+                temp = temp.next;
+            }   
+        }
+        
+        if(head.val == val) {
+            head=head.next;
+        }
+        
+        return head;
+    }
+}
